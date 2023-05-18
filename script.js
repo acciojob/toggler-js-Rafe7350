cc6c7fc2ba5ad9e8d5d22484f4bb40cac7bab224
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-05-18 15:12:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const checkboxes = document.querySelectorAll('.toggle input[type="checkbox"]');
+
+    checkboxes.forEach(checkbox => {
+      checkbox.addEventListener('change', () => {
+        let checkedCount = 0;
+        checkboxes.forEach(cb => {
+          if (cb.checked) checkedCount++;
+        });
+
+        if (checkedCount > 2) {
+          checkbox.checked = false;
+        }
+      });
+    });
